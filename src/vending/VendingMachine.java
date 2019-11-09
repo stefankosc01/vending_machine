e2342135a380e90bb922ad161a1ed66a67876195
Stefan Kość <stefan.kosc@mc2innovations.com>
2019-11-09 15:49:35 +0100

    interface refactor

--- src/vending/VendingMachine.java
+++ src/vending/VendingMachine.java
@@ -5,7 +5,6 @@ import java.util.List;
 public interface VendingMachine {
     void selectProduct(Product product);
     void insertCoin(Coin coin);
-    Product collectProduct();
-    List<Coin> collectChange();
+    Bucket<Product, List<Coin>> collectProductAndChange();
     List<Coin> refund();
 }
