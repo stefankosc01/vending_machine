826b17dbae6d991efa81c3846b26c7bdf5c3a111
Stefan Kość <stefan.kosc@mc2innovations.com>
2019-11-10 09:53:42 +0100

    added method to init with less than 73 gr

--- src/vending/VendingMachineFactory.java
+++ src/vending/VendingMachineFactory.java
@@ -1,12 +1,26 @@
 package vending;
 
+import java.util.ArrayList;
+
 public class VendingMachineFactory {
     public static VendingMachine createVendingMachine() {
         return new VendingMachineImpl();
     }
-    public static VendingMachine createVendingMachineWithExcludedDenomination() {
-        return new VendingMachineImpl(Coin.PIECGROSZY.getDenomination());
+    public static VendingMachine createVendingMachineWithExcluded5gr() {
+        ArrayList<Coin> excluded = new ArrayList<>();
+        excluded.add(Coin.PIECGROSZY);
+        return new VendingMachineImpl(excluded);
     }
+
+    public static VendingMachine createVendingMachineWithLessThan73gr() {
+        ArrayList<Coin> excluded = new ArrayList<>();
+        excluded.add(Coin.PIECDZIESIATGROSZY);
+        excluded.add(Coin.JEDENZLOTY);
+        excluded.add(Coin.DWAZLOTE);
+        excluded.add(Coin.PIECZLOTYCH);
+        return new VendingMachineImpl(excluded);
+    }
+
 
 
 }
